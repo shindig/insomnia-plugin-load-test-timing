24fb2d5954fe9143f2cc0bc4c0421211e0739c1b
Civan Yavuzsen <civanyavuzsen@gmail.com>
2021-01-28 00:22:41 +0300

    refactor: Separate action function

--- load-test-timing.js
+++ load-test-timing.js
@@ -31,152 +31,153 @@ const endHtml = `
  </body>
 </html>`;
 
-module.exports.requestGroupActions = [
-  {
-    label: "Load Test",
-    action: async (context, data) => {
-      let numIterations, delayBetweenRequests, runInParallel;
+const action = async (context, data) => {
+  let numIterations, delayBetweenRequests, runInParallel;
 
-      try {
-        const numIterationsPrompt = await context.app.prompt(
-          "How many iterations? (1/3)",
-          {
-            label: "# Iterations",
-            defaultValue: "10",
-            cancelable: true,
-            submitName: "Next",
-          },
-        );
+  try {
+    const numIterationsPrompt = await context.app.prompt(
+      "How many iterations? (1/3)",
+      {
+        label: "# Iterations",
+        defaultValue: "10",
+        cancelable: true,
+        submitName: "Next",
+      },
+    );
 
-        numIterations = parseInt(numIterationsPrompt);
+    numIterations = parseInt(numIterationsPrompt);
 
-        const delayBetweenRequestsPrompt = await context.app.prompt(
-          "Second delay between requests? (2/3)",
-          {
-            label: "# Seconds",
-            defaultValue: "1",
-            cancelable: true,
-            submitName: "Next",
-          },
-        );
+    const delayBetweenRequestsPrompt = await context.app.prompt(
+      "Second delay between requests? (2/3)",
+      {
+        label: "# Seconds",
+        defaultValue: "1",
+        cancelable: true,
+        submitName: "Next",
+      },
+    );
 
-        delayBetweenRequests = parseInt(delayBetweenRequestsPrompt);
+    delayBetweenRequests = parseInt(delayBetweenRequestsPrompt);
 
-        const bStr = (
-          await context.app.prompt("Run all requests in parallel? (3/3)", {
-            label: "Run in parallel (Y/N)",
-            defaultValue: "N",
-            cancelable: true,
-          })
-        )
-          .toLowerCase()
-          .trim();
+    const bStr = (
+      await context.app.prompt("Run all requests in parallel? (3/3)", {
+        label: "Run in parallel (Y/N)",
+        defaultValue: "N",
+        cancelable: true,
+      })
+    )
+      .toLowerCase()
+      .trim();
 
-        runInParallel =
-          bStr === "y" || bStr === "yes" || bStr === "true" || bStr === "1";
-      } catch (err) {
-        if (!err.message || !err.message.endsWith("cancelled")) {
-          context.app.alert("Unknown Error Occurred", err.message || "?");
-          console.log(err);
-        }
+    runInParallel =
+      bStr === "y" || bStr === "yes" || bStr === "true" || bStr === "1";
+  } catch (err) {
+    if (!err.message || !err.message.endsWith("cancelled")) {
+      context.app.alert("Unknown Error Occurred", err.message || "?");
+      console.log(err);
+    }
 
-        return;
-      }
+    return;
+  }
 
-      if (!numIterations) {
-        return;
-      }
+  if (!numIterations) {
+    return;
+  }
 
-      const header = `
+  const header = `
       <header>
         <b># Iterations:</b> [${numIterations}] <b>Delay between requests:</b> [${delayBetweenRequests}s] <b>Run:</b> 
         [${runInParallel ? "in Parallel" : "Serially"}]
       </header>`;
 
-      try {
-        const { requests } = data;
-        const results = requests.map((_) => {
-          return {
-            successes: 0,
-            total: 0,
-          };
-        });
+  try {
+    const { requests } = data;
+    const results = requests.map((_) => {
+      return {
+        successes: 0,
+        total: 0,
+      };
+    });
 
-        const recorder = (responses, j) => {
-          responses.forEach((response, i) => {
-            if (response.statusCode.toString().startsWith("2")) {
-              const result = results[j || i];
-              result.successes++;
-              result.total += response.elapsedTime;
-            }
-          });
-        };
+    const recorder = (responses, j) => {
+      responses.forEach((response, i) => {
+        if (response.statusCode.toString().startsWith("2")) {
+          const result = results[j || i];
+          result.successes++;
+          result.total += response.elapsedTime;
+        }
+      });
+    };
 
-        const sendRequests = (reqs) => {
-          return Promise.all(reqs.map((r) => context.network.sendRequest(r)));
-        };
+    const sendRequests = (reqs) => {
+      return Promise.all(reqs.map((r) => context.network.sendRequest(r)));
+    };
 
-        const execute = () => {
-          return new Promise((resolve) => {
-            const runIt = async (currentIteration) => {
-              console.log("Run # " + (currentIteration + 1));
+    const execute = () => {
+      return new Promise((resolve) => {
+        const runIt = async (currentIteration) => {
+          console.log("Run # " + (currentIteration + 1));
 
-              if (runInParallel) {
-                recorder(await sendRequests(requests));
-              } else {
-                for (let j = 0; j < requests.length; j++) {
-                  recorder(await sendRequests([requests[j]]), j);
-                }
-              }
-
-              if (currentIteration < numIterations - 1) {
-                setTimeout(async () => {
-                  await runIt(currentIteration + 1);
-                }, delayBetweenRequests);
-              } else {
-                return resolve();
-              }
-            };
-
-            runIt(0);
-          });
-        };
-
-        await execute();
-
-        const rows = [];
-
-        results.forEach((r, i) => {
-          const request = requests[i];
-          const result = results[i];
-          const avg = result.total / result.successes;
-
-          let color = "";
-
-          if (result.successes === numIterations) {
-            color = "limegreen";
+          if (runInParallel) {
+            recorder(await sendRequests(requests));
           } else {
-            color = "red";
+            for (let j = 0; j < requests.length; j++) {
+              recorder(await sendRequests([requests[j]]), j);
+            }
           }
 
-          rows.push(
-            `<tr>
+          if (currentIteration < numIterations - 1) {
+            setTimeout(async () => {
+              await runIt(currentIteration + 1);
+            }, delayBetweenRequests);
+          } else {
+            return resolve();
+          }
+        };
+
+        runIt(0);
+      });
+    };
+
+    await execute();
+
+    const rows = [];
+
+    results.forEach((r, i) => {
+      const request = requests[i];
+      const result = results[i];
+      const avg = result.total / result.successes;
+
+      let color = "";
+
+      if (result.successes === numIterations) {
+        color = "limegreen";
+      } else {
+        color = "red";
+      }
+
+      rows.push(
+        `<tr>
               <td>${request.name || request.url}</td>
               <td><font color="${color}">${numIterations}</font></td>
               <td>${avg.toFixed(1)}</td>
               <td>${result.total.toFixed(2)}</td>
             </tr>`,
-          );
-        });
+      );
+    });
 
-        const html =
-          startHtml + header + startTableHtml + rows.join("") + endHtml;
+    const html = startHtml + header + startTableHtml + rows.join("") + endHtml;
 
-        context.app.showGenericModalDialog("Results", { html });
-      } catch (err) {
-        context.app.alert("Unknown Error Occurred", err.message);
-        console.log(err);
-      }
-    },
+    context.app.showGenericModalDialog("Results", { html });
+  } catch (err) {
+    context.app.alert("Unknown Error Occurred", err.message);
+    console.log(err);
+  }
+};
+
+module.exports.requestGroupActions = [
+  {
+    label: "Load Test",
+    action,
   },
 ];
